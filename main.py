70c08f31555f9a8cf00b2d23b07aacd4ca28b301
Outrentice <stade42@mail.ru>
2023-05-11 14:05:10 +0300

    main -> The profile is based on data from the database, the ability to create a playlist des_Login_form -> Playlists in sidebar in Scroll Area, Add Dialog window(create playlist)

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ class LoginWindow(QtWidgets.QWidget):
         super().__init__(parent)
         self.ui = UiLoginForm()
         self.ui.setupUi(self)
+
         self.ui.sign_up_push_button.setStyleSheet(sign_up_button_style)
         self.ui.sign_in_push_button.clicked.connect(self.sign_in)
         self.ui.sign_up_push_button.clicked.connect(self.sign_up)
         self.ui.error_label.setStyleSheet(error_label_register_style)
+
         self.connection = connect(
             user="postgres",
             password="445467qwe",
@@ -37,14 +39,14 @@ class LoginWindow(QtWidgets.QWidget):
         else:
             self.ui.password_line_edit.setStyleSheet(main_style_line_edit)
 
-        self.cursor.execute('SELECT login, password FROM users WHERE login = %s',
+        self.cursor.execute('SELECT login, password, id FROM users WHERE login = %s',
                             (self.ui.login_line_edit.text(), ))
         select_res = self.cursor.fetchall()
         if select_res:
             self.ui.login_line_edit.setStyleSheet(main_style_line_edit)
             user_password = select_res[0][1]
-            if user_password == line_edit_password:
-                print('Вы вошли')
+            if user_password == hashlib.md5(line_edit_password.encode()).hexdigest():
+                self.show_main_window(select_res[0][2])
             else:
                 self.error_handler('Неверный пароль', self.ui.password_line_edit)
         else:
@@ -61,16 +63,24 @@ class LoginWindow(QtWidgets.QWidget):
         self.sign_up_window.show()
         self.hide()
 
+    def show_main_window(self, user_id):
+        self.user_profile_window = UserProfileWindow(connection=self.connection, user_id=user_id)
+        self.user_profile_window.show()
+        self.hide()
+
 
 class SignUpWindow(QtWidgets.QWidget):
     def __init__(self, parent=None, *args, **kwargs):
         super().__init__(parent)
         self.ui = UiSignUpForm()
         self.ui.setupUi(self)
+
         self.ui.sign_up_register_push_button.clicked.connect(self.sign_up_reg)
         self.ui.login_register_line_edit.setText(kwargs['login_line_edit'])
+
         self.connection = kwargs['connection']
         self.cursor = self.connection.cursor()
+
         self.ui.error_label.setStyleSheet(error_label_register_style)
 
     def sign_up_reg(self):
@@ -133,6 +143,10 @@ class SignUpWindow(QtWidgets.QWidget):
                             (user_email, hashlib.md5(user_password.encode()).hexdigest(), user_login))
         self.connection.commit()
 
+        self.user_profile_window = UserProfileWindow(connection=self.connection)
+        self.user_profile_window.show()
+        self.hide()
+
     def error_handler(self, error_text: str, obj: QtWidgets.QLineEdit):
         obj.setStyleSheet(error_line_edit_register_style) #Переданному line_edit присваиваются стили ошибки(Красная рамка)
         self.ui.error_label.setText(error_text)
@@ -140,12 +154,101 @@ class SignUpWindow(QtWidgets.QWidget):
     def check_data(self, error_desc: str, column_name: str, obj_line_edit: QtWidgets.QLineEdit) -> bool:
         self.cursor.execute(f'SELECT {column_name} FROM users WHERE {column_name} = %s', (obj_line_edit.text().strip(),))
         checker = self.cursor.fetchone()
+        print(checker)
         if checker: #Если запрос к базе вернет кортеж, то поле в базе существует
             self.error_handler(error_desc, obj_line_edit)
             return True
         else:
             obj_line_edit.setStyleSheet(main_style_line_edit)
             return False
+
+
+class UserProfileWindow(QtWidgets.QWidget):
+    def __init__(self, parent=None, *args, **kwargs):
+        super().__init__(parent)
+        self.ui = UiUserProfile()
+        self.ui.setupUi(self)
+
+        self.connection = kwargs['connection']
+        self.user_id = kwargs['user_id']
+        self.cursor = self.connection.cursor()
+
+        self.ui.playlist1_into_scroll.clicked.connect(self.click_button_playlist)
+        self.ui.pushButton.clicked.connect(self.window_create_playlist)
+
+        self.set_user_data()
+
+    def create_playlist_button(self):
+        name_playlist = self.dialog_create_playlist.lineEdit.text().strip()
+
+        if name_playlist:
+            self.cursor.execute('SELECT playlist_name FROM playlist WHERE playlist_name = %s', (name_playlist, ))
+            check_unique_name = self.cursor.fetchone()
+
+            if not check_unique_name:
+                self.cursor.execute('INSERT INTO playlist (playlist_name, user_id) VALUES (%s, %s)', (name_playlist, self.user_id))
+                self.connection.commit()
+                self.dialog_window.close()
+
+                if self.ui.horizontalLayout_playlist.count() < 3:
+                    new_playlist = QtWidgets.QPushButton()
+                    new_playlist.setMinimumSize(QtCore.QSize(150, 150))
+                    new_playlist.setMaximumSize(QtCore.QSize(150, 150))
+                    new_playlist.setStyleSheet("border: 1px solid black; border-radius: 7%;"
+                                               "background-color: rgb(89, 114, 93);")
+                    new_playlist.setObjectName(name_playlist + '_into_main')
+                    new_playlist.setText(name_playlist)
+                    self.ui.horizontalLayout_playlist.addWidget(new_playlist)
+            else:
+                self.error_handler('Плейлист существует',
+                                   self.dialog_create_playlist.lineEdit)
+
+    def window_create_playlist(self):
+        self.dialog_window = QtWidgets.QDialog()
+        self.dialog_create_playlist = UiCreatePlaylist()
+        self.dialog_create_playlist.setupUi(self.dialog_window)
+
+        self.dialog_create_playlist.pushButton_Create.clicked.connect(self.create_playlist_button)
+
+        self.dialog_window.exec()
+
+    def set_user_data(self):
+        self.cursor.execute('SELECT * FROM users WHERE id = %s', (self.user_id, ))
+        user_data = self.cursor.fetchone()
+        self.ui.username.setText('username: ' + user_data[3])
+
+        self.cursor.execute('SELECT * FROM playlist WHERE user_id = %s', (self.user_id,))
+        playlists_data = self.cursor.fetchall()
+
+        self.ui.playlist1_into_scroll.setText(playlists_data[0][1])
+        self.ui.playlist1_into_main.setText(playlists_data[0][1])
+
+        for playlist in playlists_data[1:]:
+            count_main = self.ui.horizontalLayout_playlist.count()
+
+            new_playlist = QtWidgets.QPushButton()
+            new_playlist.setMinimumSize(QtCore.QSize(150, 150))
+            new_playlist.setMaximumSize(QtCore.QSize(150, 150))
+            new_playlist.setStyleSheet("border: 1px solid black; border-radius: 7%;" 
+                                       "background-color: rgb(62, 79, 65);")
+            new_playlist.setObjectName(playlist[1] + '_into_scroll')
+            new_playlist.setText(playlist[1])
+
+            self.ui.verticalLayout_in_scroll.addWidget(new_playlist)
+
+            if count_main < 3:
+                new_playlist.setStyleSheet("border: 1px solid black; border-radius: 7%;"
+                                           "background-color: rgb(89, 114, 93);")
+                new_playlist.setObjectName(playlist[1] + '_into_main')
+                self.ui.horizontalLayout_playlist.addWidget(new_playlist)
+
+    def click_button_playlist(self):
+        print('Привет')
+
+    def error_handler(self, error_text: str, obj: QtWidgets.QLineEdit):
+        obj.setStyleSheet(error_line_edit_register_style) #Переданному line_edit присваиваются стили ошибки(Красная рамка)
+        self.dialog_create_playlist.error_label.setText(error_text)
+
 
 
 if __name__ == "__main__":
